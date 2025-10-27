421644ec3ef98fd1c726af5412f249819405efc3
m.kapuscinski <m.kapuscinski@ideo.pl>
2025-10-27 20:03:14 +0100

    feat(backend): connect to MongoDB Atlas and add user CRUD with async error handling

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,14 @@
 import express from "express";
 
 import { errorHandler } from "./middlewares/errorHandler";
-import itemRoutes from "./routes/itemRoutes";
+import userRoutes from "./routes/userRoutes";
 import { AppError } from "./utils/AppError";
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/api/items", itemRoutes);
+app.use("/api/users", userRoutes);
 
 app.use((req, _res, next) => {
 	next(new AppError(`Route ${req.originalUrl} not found`, 404));
---
+++ backend/src/controllers/userController.ts
@@ -0,0 +1,65 @@
+import type { NextFunction, Request, Response } from "express";
+
+import { User } from "../models/userModel";
+import { AppError } from "../utils/AppError";
+import { catchAsync } from "../utils/catchAsync";
+
+const createUser = catchAsync(
+	async (req: Request, res: Response, _next: NextFunction) => {
+		const newUser = await User.create(req.body);
+
+		res.status(201).json(newUser);
+	},
+);
+
+const getAllUsers = catchAsync(
+	async (_req: Request, res: Response, _next: NextFunction) => {
+		const users = await User.find();
+
+		res.status(200).json(users);
+	},
+);
+
+const getUserById = catchAsync(
+	async (req: Request, res: Response, next: NextFunction) => {
+		const user = await User.findById(req.params.id);
+
+		if (!user) {
+			return next(
+				new AppError(`No user found with that ID: ${req.params.id}`, 404),
+			);
+		}
+
+		res.status(200).json(user);
+	},
+);
+
+const updateUser = catchAsync(
+	async (req: Request, res: Response, next: NextFunction) => {
+		const user = await User.findByIdAndUpdate(req.params.id);
+
+		if (!user) {
+			return next(
+				new AppError(`No user found with that ID: ${req.params.id}`, 404),
+			);
+		}
+
+		res.status(200).json(user);
+	},
+);
+
+const deleteUser = catchAsync(
+	async (req: Request, res: Response, next: NextFunction) => {
+		const user = await User.findByIdAndDelete(req.params.id);
+
+		if (!user) {
+			return next(
+				new AppError(`No user found with that ID: ${req.params.id}`, 404),
+			);
+		}
+
+		res.status(204).send();
+	},
+);
+
+export default { createUser, getAllUsers, getUserById, updateUser, deleteUser };
