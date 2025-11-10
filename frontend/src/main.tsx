ccaa1c53fbc3f8917ccd492c3058a197bbc7e9ba
Mateusz Kapuściński <kapuscinski.mateusz01@gmail.com>
2025-11-10 18:01:47 +0100

    feat(frontend): add react-query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,5 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
@@ -13,13 +15,18 @@ declare module "@tanstack/react-router" {
 	}
 }
 
+const queryClient = new QueryClient();
+
 // biome-ignore lint/style/noNonNullAssertion: root element is guaranteed to exist
 const rootElement = document.getElementById("root")!;
 if (!rootElement.innerHTML) {
 	const root = ReactDOM.createRoot(rootElement);
 	root.render(
 		<StrictMode>
-			<RouterProvider router={router} />
+			<QueryClientProvider client={queryClient}>
+				<RouterProvider router={router} />
+				<ReactQueryDevtools initialIsOpen={false} />
+			</QueryClientProvider>
 		</StrictMode>,
 	);
 }
